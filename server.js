290d3109eb9f2c17ae8e82dbff75c6dac9cc32ea
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-04-02 23:40:45 -0300

    create join page

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve() + "/public/";
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.get("/chat", (req, res) => {
+    res.sendFile(__dirname + "pages/chat.html");
 });
 
 app.listen(3000, () => console.log("server rodando na porta 3000"));
